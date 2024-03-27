db3c696df6d7e374707d2ce8e721ca0fc527b081
rupert van niekerk <127295503+rupert08@users.noreply.github.com>
2024-03-27 12:49:52 +0200

    Delete src/main/java/za/ac/cput/domain/factory directory

--- src/main/java/za/ac/cput/domain/factory/InventoryFactory.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.domain.factory;
-
-import za.ac.cput.domain.Inventory;
-
-public class InventoryFactory {
-    public static  Inventory buildInvertory(String inventoryId,String name,String location,String stockItemId){
-        return new Inventory.Builder().setInventoryId(inventoryId).setName(name).setLocation(location).setStockItemId(stockItemId).build();
-    }
-}
