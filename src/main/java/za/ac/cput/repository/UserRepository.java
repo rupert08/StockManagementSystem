7443087117911702b3fc93398bd259c76b04bdd2
Raymond2904 <127522052+Raymond2904@users.noreply.github.com>
2024-03-27 17:35:52 +0200

    version1

--- src/main/java/za/ac/cput/repository/UserRepository.java
+++ src/main/java/za/ac/cput/repository/UserRepository.java
@@ -10,6 +10,7 @@ Author: Raymond van Niekerk (221154469)
 Date:  March 2024
  */
 public class UserRepository implements IUserRepository {
+
     private List<User> users;
 
     public UserRepository() {
@@ -17,12 +18,12 @@ public class UserRepository implements IUserRepository {
     }
 
     @Override
-    public void addUser(User user) {
+    public void add(User user) {
         users.add(user);
     }
 
     @Override
-    public User getUserById(String userId) {
+    public User getById(String userId) {
         for (User user : users) {
             if (user.getUserId().equals(userId)) {
                 return user;
@@ -32,25 +33,51 @@ public class UserRepository implements IUserRepository {
     }
 
     @Override
-    public void updateUser(User user) {
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User read(String s) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getUserId().equals(user.getUserId())) {
                 users.set(i, user);
                 break;
             }
         }
+        return user;
     }
 
     @Override
-    public boolean deleteUser(String userId) {
-        User userToDelete = getUserById(userId);
+    public boolean delete(String userId) {
+        User userToDelete = getById(userId);
         if (userToDelete == null)
             return false;
         return users.remove(userToDelete);
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public List<User> getAll() {
         return new ArrayList<>(users);
+    }
+
+    @Override
+    public User getUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getRole(String role) {
+        return null;
+    }
+
+    @Override
+    public void addUser(User user) {
+
     }
 }
